e87b83e6edef43cbe46eb6b736250e8d57aea042
jeounghaa <43715735+jeounghaa@users.noreply.github.com>
2020-09-22 17:51:16 +0900

    Add files via upload

---
+++ java37/src/형변환Casting/VO.java
@@ -0,0 +1,23 @@
+package Çüº¯È¯Casting;
+
+public class VO {
+	String id;
+	String pw;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+}
